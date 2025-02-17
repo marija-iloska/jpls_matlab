330ce5242a2d8e0f00951d3317f06f18d4e7ecda
Marija Iloska <marija.iloska@stonybrook.edu>
2025-02-16 19:15:09 -0500

    match notation and simpler implementation

--- tpls/tpls.m
+++ tpls/tpls.m
@@ -1,115 +1,138 @@
-function [theta_k, idx_jpls, J_pred, plot_stats, idx_store] = tpls(y, H, K, var_y, t0, idx_h)
+function [theta_k, S_features_used, J_pred, plot_stats] = tpls(y, H, k, n0, idx)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % This fn is a compact implementation of TPLS only for the purpose of
 % generating statistical experiments comparing to other methods. Users
 % should see example_code.m for online implementation of TPLS
 
-% Store
-H_true = H;
-T = length(y);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Get estimate using half the total features
-k = floor(K/2);
-%e = [];
-[~, ~, theta_k, Dk, ~,~] = initialize(y, H, t0, k, var_y);
+% Dimensions
+N = length(H(:,1));
+K = length(H(1,:));
 
-% Initialize variables
-J_pred = [];
-J = 0;
+% Set S 
+S_features_used = datasample(1:K, k, 'replace', false);
+S_features_unused = setdiff(1:K, S_features_used);
+
+% Initial batch of data
+y0 = y(1:n0);
+H0 = H(1:n0,:);
+
+[theta_k, Dk] = initialize(y0, H0(:,S_features_used));
+model_at_n0 = {theta_k, Dk};
 
 % Model storage
-correct = zeros(1,T-t0);
-incorrect = zeros(1,T-t0);
-idx_store ={};
+correct = zeros(1,N-n0);
+incorrect = zeros(1,N-n0);
+
+% Predictive errors init
+J = 0;
+J_pred = [];
+J_above = Inf;
+J_below = Inf;
 
 
-% Parameter estimate storage
-%theta_store = {};
-
-% Dk matrix storage
-Dk_jump = {Dk, Dk, Dk};
-
-% Set of all feature indices
-idx_H = 1:K;
-
-
+%% JPLS LOOP
 % Start time loop
-for t = t0+1:T
+tic
+for n = n0+1:N
 
-    %% SETUP
+    % Data
+    Hn = H(1:n, S_features_used);
+    yn = y(1:n);
 
-    % Update to J(k,t) from J(k,t-1)
-    J = J + (y(t) - H(t, 1:k)*theta_k)^2; 
+    % CURRENT MODEL PE
+    J = J + (y(n) - Hn(n,:)*theta_k)^2;
 
-    % Collect current states theta_(k, t-1) J(k,t), Dk(k, t-1)
-    stay = {theta_k, idx_H, J, Dk, k};
-    
-    
-    % Reset PE storage
-    J_jump = {J, Inf, Inf};
-
-
-    %% MOVES 
-
-    % STAY SAME
-    [theta_jump{1}, idx_jump{1}, J_jump{1}, Dk_jump{1}, k_jump{1}] = stay{:};
-
-    % JUMP UP +
-    if (K > k)
-        [theta_jump{2}, idx_jump{2}, J_jump{2}, Dk_jump{2}, k_jump{2}] = jump_up(y, K, k, Dk, theta_k, J, H, t, t0, var_y) ;
+    % MODEL ABOVE PE
+    if k < K
+        rm_idx = nan;
+        parfor m = 1:K-k
+             difference = pred_error_model_above(yn, [Hn H(1:n, S_features_unused(m))], n, n0, model_at_n0);
+             J_above(m) = J + difference;     
+        end
     end
 
-    % JUMP DOWN -
-    if (k > 1)
-        [theta_jump{3}, idx_jump{3}, J_jump{3}, Dk_jump{3}, k_jump{3}] = jump_down(y, k, Dk, theta_k, J, H, t, t0, var_y, K);
+   % MODEL BELOW PE
+   if k > 1
+    parfor m = 1:k
+         difference = pred_error_model_below(yn, Hn, n, n0, m, model_at_n0);
+         J_below(m) = J + difference;
+    end
+   end
+
+    % Find min PE
+    m_above  = find(J_above == min(J_above));
+    m_below = find(J_below == min(J_below));
+    minJ = min([J_above(m_above), J_below(m_below), J]);
+
+    % IF BELOW
+    if minJ == J_below(m_below)
+
+
+        % Update base model
+        [theta0, D0] = descendingORLS(model_at_n0{1}, model_at_n0{2}, m_below);
+        model_at_n0 = {theta0, D0};
+
+
+        % Move to neighbor model below
+        [theta_k, Dk] = descendingORLS(theta_k, Dk, m_below);
+
+        % Update predictive error
+        J = J_below(m_below);
+
+        % Update feature sets
+        S_features_unused(end+1) = S_features_used(m_below);
+        S_features_used(m_below) = [];
+
+        % Update model dimension
+        k = k - 1;
+
+    % IF ABOVE
+    elseif minJ == J_above(m_above)     
+
+        % Update base model
+        [theta0, D0] = ascendingORLS(y0, H0(:,S_features_used), H0(:, S_features_unused(m_above)), n0, model_at_n0{2}, model_at_n0{1});
+        model_at_n0 = {theta0, D0};
+
+        % Move to neighbor model above
+        [theta_k, Dk] = ascendingORLS(yn(1:n-1), Hn(1:n-1,:), H(1:n-1, S_features_unused(m_above)), n-1, Dk, theta_k);
+        
+        % Update predictive error
+        J = J_above(m_above);
+
+        % Update feature sets
+        S_features_used(end+1) = S_features_unused(m_above);
+        S_features_unused(m_above) = [];
+
+        % Update model dimension
+        k = k + 1;
+
     end
 
+    %[J_scratch] = normal_PE(y, H, S_features_used,n0, n);
 
-    %% CRITERION CHOICE
-    % Find Model with lowest PredError
-    Js = [J_jump{1}, J_jump{2}, J_jump{3}];
-    minJ = find(Js == min(Js));
-
-
-    % Assign quantities to chosen model: all(t-1)
-    H = H(:, idx_jump{minJ});
-    k = k_jump{minJ};
-    Dk = Dk_jump{minJ};
-    theta_k = theta_jump{minJ};
-    J = J_jump{minJ};
-
-
-    %% QUANTITIES UPDATES
-
-    % Update and store terms
-    Hk = H(1:t, 1:k);
-    %theta_store{end+1} = theta_k;
-
-    % PREDICTIVE ERROR STORAGE
+    % Place holders
+    J_above = Inf;
+    J_below = Inf;
+    
+    % Store predictive error
     J_pred(end+1) = J;
-    %e(end+1) = y(t) - H(t, 1:k)*theta_k; 
 
-    %% EVALUATION
 
-    % Check which model was selected at time t and store
-    [~, idx_jpls] = ismember(Hk(1,:), H_true(1,:));
+    % TIME UPDATE    
+    % theta(k,n) <-- theta(k,n-1) and Dk(n) <-- Dk(n-1)
+    [theta_k, Dk] = RLS(y(n), H(n,S_features_used), theta_k, Dk);
 
-    % Evaluate features
-    correct(t-t0) = sum(ismember(idx_jpls, idx_h));
-    incorrect(t-t0) = length(idx_jpls) - correct(t-t0);
-
-    % Store current model (feature indices)
-    idx_store{end+1} = idx_jpls;
-
-    %% TIME UPDATE 
-    
-    % theta(k,t) from theta(k,t-1) and Dk(t) from Dk(t-1)
-    [theta_k, Dk] = time_update(y(t), Hk(t,:), theta_k, var_y, Dk);
-
+    % EVALUATION
+    correct(n-n0) = sum(ismember(S_features_used, idx));
+    incorrect(n-n0) = length(S_features_used) - correct(n-n0);
 
 
 end
-
+toc
 % Concatenate results
 plot_stats = {correct, incorrect};
 
