fc69cfe0f1361255c56164602fc9210da6010759
Marija Iloska <marija.iloska@stonybrook.edu>
2025-02-16 19:14:42 -0500

    simplify indices

--- util/initialize.m
+++ util/initialize.m
@@ -1,25 +1,11 @@
-function [J, e, theta_k, Dk, Hk, Sigma] = initialize(y, H, t, k, var_y)
-
-
-% Initialize first Hk
-Hk = H(1:t,1:k);
+function [theta_k, Dk] = initialize(y, H)
 
 
 % Initialize first Dk
-Dk = inv(Hk'*Hk);
+Dk = inv(H'*H);
 
 % Compute iniital estimate of theta_k
-theta_k = Dk*Hk'*y(1:t);
+theta_k = Dk*H'*y;
 
-% Initial covariance of data
-Sigma = Dk/var_y;
-
-% Initial predictive and residual predictive error
-J = sum((y(1:t+1) - H(1:t+1, 1:k)*theta_k).^2);
-e = y(t+1) - H(t+1, 1:k)*theta_k;
-
-
-% Initialize first Hk
-Hk = H(:,1:k);
 
 end
